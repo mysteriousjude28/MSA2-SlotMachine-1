42f7779c4f352cab07959d7e92b8e26a29725a34
Forest Newark <forest.newark@gmail.com>
2017-03-10 13:31:44 -0500

    added SlotMachine class

---
+++ src/sample/SlotMachine.java
@@ -0,0 +1,38 @@
+package sample;
+
+import java.util.Random;
+
+/**
+ * Created by forestnewark on 3/10/17.
+ */
+public class SlotMachine {
+
+    private int wheelOne;
+    private int wheelTwo;
+    private int wheelThree;
+
+    private void setWheels(){
+        Random random = new Random();
+        wheelOne = random.nextInt(10) +1;
+        wheelTwo = random.nextInt(10) +1;
+        wheelThree = random.nextInt(10) +1;
+    }
+
+    public void play() {
+        setWheels();
+    }
+
+     public boolean didPlayerWin(){
+        if (wheelOne == wheelTwo && wheelTwo == wheelThree) {
+            return true;
+        }
+     }
+
+     public boolean didBonusHit(){
+         if (wheelOne == 7 && wheelTwo == 7 && wheelThree == 7) {
+             return true;
+
+         }
+     }
+    
+}
