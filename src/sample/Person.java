8276f8e429622fadf536fd608ccba590d8162606
James Hartanto <jameshartanto1993@gmail.com>
2017-03-10 15:01:13 -0500

    Fixed logic and created currentMoney method

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -9,49 +9,40 @@ public class Person {
     public String name;
     public int money;
     public int bet;
-    private boolean broke;
 
     public Person(String name, int money) {
         this.name = name;
         this.money = money;
     }
 
-    /* exists in SlotMachine.java
-    // Slot Machine Numbers
-    Random slotNumbers = new Random();
-    int slotNumber1 = slotNumbers.nextInt(9)+1;
-    int slotNumber2 = slotNumbers.nextInt(9)+1;
-    int slotNumber3 = slotNumbers.nextInt(9)+1;
-    */
-
-    // Player place bet and shows how much money they have left
-    public int setBet(Person person) {
-        // Controller place bet
-        int playerBet = 5; // THIS NEEDS TO CONNECT THE TO SLOT MACHINE INTERFACE
-        // Slot machine numbers
-//        System.out.printf("%s, %s, %s", slotNumber1, slotNumber2, slotNumber3);
-        // Winnings and amount of money left
-        int winnings = slotResults(playerBet, boolean slotMachine.didPlayerWin(), boolean slotMachine.didBonusHit());
-        return (person.money = person.money + winnings - playerBet);
-//        // Message for user
-//        forceLeave(person);
-//        System.out.println("You have " + person.money + " dollars left. " + security(message));
+    public int currentMoney(Person person, int betAmount){
+        int winnings = setBet(betAmount);
+        return (person.money - betAmount + winnings);
     }
 
+    // Player place bet and sees if they win
+    public int setBet(int betAmount) {
+        int playerBet = betAmount;
+        int winnings = slotResults(betAmount, slotMachine.didPlayerWin(), slotMachine.didBonusHit());
+        return winnings;
+    }
 
     // Slot machine results
     public int slotResults(int betAmount, boolean slotMachine.didPlayerWin(), boolean slotMachine.didBonusHit()) {
         if (slotMachine.didBonusHit() == true) {
-//            System.out.println("Congratulations! You won " + (betAmount * 2) + " dollars!");
             return betAmount * 2;
         } else if (didPlayerWin() == true) {
-//            System.out.println("Congratulations! You got " + (betAmount) + " dollars back!");
             return betAmount;
         } else {
-//            System.out.println("Better luck next time!");
             return 0;
         }
     }
+
+
+
+//        // Message for user
+//        forceLeave(person);
+//        System.out.println("You have " + person.money + " dollars left. " + security(message));
 
 //    // Message after each bet
 //    String message = " ";
