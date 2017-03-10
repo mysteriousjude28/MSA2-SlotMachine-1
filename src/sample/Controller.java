f828d11b6f13726b4f76310172366e04d218981e
Forest Newark <forest.newark@gmail.com>
2017-03-10 14:37:07 -0500

    Started layout file

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    SlotMachine slotMachine = new SlotMachine();
+
+    public void play(ActionEvent actionEvent) {
+        slotMachine.play();
+    }
 }
