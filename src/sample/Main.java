b28358215961cacf75afbc6f554de653ccf4db01
Tiambe Hooper <tiambehooper@users.noreply.github.com>
2017-03-11 15:31:44 -0500

    Font end technology??
    Why does this Project say hello wold for a slot machine?? Main?? Would you like to add any back-end and logical and methods the assuagement required?

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,17 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+// Too extend an application
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+      
+      //Root Directory
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello World");//S
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
-
+//entry point ot the application
     public static void main(String[] args)
     {
         launch(args);
